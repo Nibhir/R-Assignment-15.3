f2e02f5becfef73a864322a17a45b9f0a50a2378
Nibhir Ghosh <30548445+Nibhir@users.noreply.github.com>
2017-10-02 21:14:54 +0530

    Add files via upload

---
+++ Assignment 15.3.R
@@ -0,0 +1,19 @@
+#firstpart
+
+a=c(103,82,179)
+
+for(val in a)
+{
+  if(val %% 2 == 0)
+  
+    print("Not Prime")
+
+  else(print("Prime"))  
+}
+
+#secondpart
+
+b <- c("above","unit","under")
+
+grep(pattern = "a",b,value = T)
+grep(pattern = "u",b,value = T)
